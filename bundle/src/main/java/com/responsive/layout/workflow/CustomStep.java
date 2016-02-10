554c7f24ba55f00bf4b48e9437a4bfa31d097135
Dan Sheidak <dan.sheidak@6dglobal.com>
2016-02-10 14:32:19 +0300

    10.02

--- bundle/src/main/java/com/responsive/layout/workflow/CustomStep.java
+++ bundle/src/main/java/com/responsive/layout/workflow/CustomStep.java
@@ -7,19 +7,32 @@ import com.adobe.granite.workflow.exec.WorkflowData;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
 import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.commons.util.DamUtil;
+import com.day.cq.tagging.InvalidTagFormatException;
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
+import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component(immediate = true, metatype = true)
 @Service
@@ -36,26 +49,58 @@ public class CustomStep implements WorkflowProcess {
 
     private static final Logger log = LoggerFactory.getLogger(CustomStep.class);
 
-    @Self
-    Resource resource;
+    @Reference
+    private ResourceResolverFactory resourceResolverFactory;
 
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) throws WorkflowException {
         log.info("Starting CustomStep...");
-//        DamUtil.resolveToAsset(resource);
-        WorkflowData workflowData = workItem.getWorkflowData();
-//        if (workflowData.getPayloadType().equals(TYPE_JCR_PATH)) {
-            final String payloadPath = workItem.getWorkflowData().getPayload().toString();
-            try {
-                Session jcrSession = workflowSession.adaptTo(Session.class);
-                Node node = (Node) jcrSession.getItem(payloadPath + "/" + JcrConstants.JCR_CONTENT);
-//                if (node != null) {
-//                    node.setProperty(PROPERTY_OLM_APPROVED, true);
-//                    jcrSession.save();
-//                }
-            } catch (RepositoryException e) {
-                log.error(e.getMessage(), e);
+        ResourceResolver resolver = null;
+        try {
+            resolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+        } catch (LoginException e) {
+            log.error(e.getMessage());
+            return;
+        }
+        String payloadPath = workItem.getWorkflowData().getPayload().toString();
+        Resource resource = resolver.getResource(payloadPath);
+        Asset asset = DamUtil.resolveToAsset(resource);
+        String assetName = asset.getName();
+        String[] keyWords = (String[]) metaDataMap.get("keyWords");
+        for(String keyWord : keyWords) {
+            if(assetName.contains(keyWord)){
+                String newTagTitle = (String) metaDataMap.get("tag");
+                addTag(newTagTitle, asset);
             }
-//        }
+        }
+        resolver.close();
+    }
+
+    private void addTag(String newTagTitle, Asset asset){
+        ResourceResolver resolver = null;
+        try {
+            resolver = resourceResolverFactory.getAdministrativeResourceResolver(null);
+        } catch (LoginException e) {
+            log.error(e.getMessage());
+            return;
+        }
+        String path = asset.getPath();
+        Resource metadataResource = resolver.getResource(path + "/jcr:content/metadata");
+        TagManager tagManager = resolver.adaptTo(TagManager.class);
+        try {
+            Tag newTag = tagManager.createTagByTitle(newTagTitle);
+            Tag[] tags = tagManager.getTags(metadataResource);
+            List<Tag> tagsList = new ArrayList<>(Arrays.asList(tags));
+            if(!tagsList.contains(newTag)){
+                tagsList.add(newTag);
+            }
+            Tag[] updatedTags = new Tag[tagsList.size()];
+            tagsList.toArray(updatedTags);
+            tagManager.setTags(metadataResource, updatedTags);
+        } catch (InvalidTagFormatException e) {
+            log.error(e.getMessage());
+        } finally {
+            resolver.close();
+        }
     }
 }
